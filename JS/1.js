fc6173617e20bb02aa2b16802ae506db72b71281
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2024-11-07 13:29:36 +0530

    Update 1.js

--- JS/1.js
+++ JS/1.js
@@ -1,16 +1,1 @@
-let marks = prompt("enter your marks between 1 to 100 :")
-if(marks<=100 && marks>=0){
-    if(marks>=90){
-        console.log("your grade is A")
-    }else if(marks>=80 && marks<90){
-        console.log("your grade is B")
-    }else if(marks>=70 && marks<80){
-        console.log("your grade is C")
-    }else if(marks>=60 && marks<70){
-        console.log("your grade is D")
-    }else{
-        console.log("your grade is F")
-    }
-}else{
-    console.log("your marks is not valid.")
-}
+hi hello
